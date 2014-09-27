6d9b88912941e743b7f1e8d073a94ef4434f3edd
Jackson de Campos <jacksondecampos@gmail.com>
2014-09-26 21:47:21 -0700

    Use tabs for indentation

--- js/main.js
+++ js/main.js
@@ -1,60 +1,33 @@
 var foggy = true;
 var empty = true;
 
-function placeCaretAtEnd(el) {
-    el.focus();
-    if (typeof window.getSelection != "undefined"
-            && typeof document.createRange != "undefined") {
-        var range = document.createRange();
-        range.selectNodeContents(el);
-        range.collapse(false);
-        var sel = window.getSelection();
-        sel.removeAllRanges();
-        sel.addRange(range);
-    } else if (typeof document.body.createTextRange != "undefined") {
-        var textRange = document.body.createTextRange();
-        textRange.moveToElementText(el);
-        textRange.collapse(false);
-        textRange.select();
-    }
-}
-
-function getCaretPosition(editableDiv) {
-    var caretPos = 0, containerEl = null, sel, range;
-    if (window.getSelection) {
-        sel = window.getSelection();
-        if (sel.rangeCount) {
-            range = sel.getRangeAt(0);
-            if (range.commonAncestorContainer.parentNode == editableDiv) {
-                caretPos = range.endOffset;
-            }
-        }
-    } else if (document.selection && document.selection.createRange) {
-        range = document.selection.createRange();
-        if (range.parentElement() == editableDiv) {
-            var tempEl = document.createElement("span");
-            editableDiv.insertBefore(tempEl, editableDiv.firstChild);
-            var tempRange = range.duplicate();
-            tempRange.moveToElementText(tempEl);
-            tempRange.setEndPoint("EndToEnd", range);
-            caretPos = tempRange.text.length;
-        }
-    }
-    return caretPos;
-}
 
 $(document).ready(function() {
-  var out = $('.out');
+
+  var write = $('.write');
 
   Mousetrap.bind('mod+h', function(e) {
-    $('.write').toggleClass('blur');
+    write.toggleClass('blur');
     foggy = !foggy;
   });
 
-  $('.write').focus();
+  write.focus();
 
-  $('.write').keyup(function() {
-    var write = $('.write');
+  write.keydown(function(e) {
+    if (e.keyCode == 9) {
+      var myValue = "\t";
+      var startPos = this.selectionStart;
+      var endPos = this.selectionEnd;
+      var scrollTop = this.scrollTop;
+      this.value = this.value.substring(0, startPos) + myValue + this.value.substring(endPos,this.value.length);
+      this.focus();
+      this.selectionStart = startPos + myValue.length;
+      this.selectionEnd = startPos + myValue.length;
+      this.scrollTop = scrollTop;
+
+      e.preventDefault();
+    }
+
     var text = write.val();
 
     if(text && empty) {
